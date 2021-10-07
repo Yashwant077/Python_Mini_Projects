4a98fd07275195d724f866879469989f629a35e1
Yashwant077 <84570757+Yashwant077@users.noreply.github.com>
2021-10-07 11:03:11 +0530

    Create miniProject1.py
    Done

---
+++ miniProject1.py
@@ -0,0 +1,9 @@
+# Project Name: To make a Dictionary
+
+dict1 = {"Array": "Collection of similar Data types", "List": "Mutable", "Tuple":"Immutable", "Sets": "Collection of unique items"}
+
+print(dict1.keys())
+
+word = input("Enter a word to find its meaning:: ")
+print(dict1[word])
+#print(dict1.get(word))
